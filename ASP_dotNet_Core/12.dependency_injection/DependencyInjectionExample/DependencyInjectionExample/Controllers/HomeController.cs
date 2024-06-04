4e2451126080eb2617c56bb58fd35ffb6d1f9ae7
nguyenhoangan03 <nguyenhoangan03.work@gmail.com>
2024-06-04 12:35:34 +0700

    Dependency Injection

---
+++ ASP_dotNet_Core/12.dependency_injection/DependencyInjectionExample/DependencyInjectionExample/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+namespace DependencyInjectionExample.Controllers {
+    public class HomeController : Controller {
+
+        private readonly ICitiesService _citiesService;
+
+        public HomeController([FromServices]ICitiesService citiesService) {
+            // create object of CitiesService class
+            _citiesService = citiesService;//new CitiesService();
+        }
+        [Route("/")]
+        public IActionResult Index() {
+            List<string>? cities = _citiesService.GetCities();
+            return View(cities);
+        }
+    }
+}
